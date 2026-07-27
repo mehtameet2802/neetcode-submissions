31d486433e05b4921794738358a3156bea5ce181
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-07-27 15:28:30 +0530

    Add: integer-break - submission-2

---
+++ Data Structures & Algorithms/integer-break/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        mem = {}
+        
+        def helper(i, cur_sum):
+
+            if (i,cur_sum) in mem:
+                return mem[(i,cur_sum)]
+
+            if cur_sum == n:
+                return 1
+
+            if i>=n or cur_sum>n:
+                return 0
+            
+            a1 = i*helper(i, cur_sum+i)
+            a2 = helper(i+1, cur_sum)
+
+            mem[(i,cur_sum)] = max(a1, a2)
+            return mem[(i,cur_sum)]
+
+        return helper(1,0)
+
