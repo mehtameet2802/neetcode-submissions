9287fad58d251db2fef378b26dd1f83a4bea2af0
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-07-29 16:14:49 +0530

    Add: find-median-in-a-data-stream - submission-0

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-0.py
@@ -0,0 +1,35 @@
+import heapq
+
+class MedianFinder:
+
+    def __init__(self):
+        self.min_heap = []
+        self.max_heap = []
+
+    def addNum(self, num: int) -> None:
+        if not self.max_heap or num <= -self.max_heap[0]:
+            heapq.heappush(self.max_heap,-num)
+        else:
+            heapq.heappush(self.min_heap,num)
+
+        diff = abs(len(self.min_heap)-len(self.max_heap))
+
+        if diff>1:
+            if len(self.min_heap)>len(self.max_heap):
+                heapq.heappush(self.max_heap,-heapq.heappop(self.min_heap))
+            else:
+                heapq.heappush(self.min_heap,-heapq.heappop(self.max_heap))
+
+    def findMedian(self) -> float:
+        if len(self.min_heap)>len(self.max_heap):
+            return self.min_heap[0]
+        elif len(self.max_heap)>len(self.min_heap):
+            return -self.max_heap[0]
+        else:
+            return (self.min_heap[0]-self.max_heap[0]) / 2
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
