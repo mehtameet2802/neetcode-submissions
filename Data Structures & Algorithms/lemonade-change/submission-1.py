c1a4cb316ce0cc5ae2eae53b654550ad642e12c6
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-07-31 15:21:06 +0530

    Add: lemonade-change - submission-1

---
+++ Data Structures & Algorithms/lemonade-change/submission-1.py
@@ -0,0 +1,37 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+
+        '''
+        Pattern - Greedy
+
+        TC - O(n)
+        SC - O(1)
+        '''
+
+        five = 0
+        ten = 0
+
+        for bill in bills:
+
+            if bill == 5:
+                five+=1
+            
+            elif bill == 10:
+                ten += 1
+
+                if five == 0:
+                    return False
+                
+                five -= 1
+                
+            else:
+                if ten>=1 and five>=1:
+                    ten -= 1
+                    five -= 1
+                elif five>=3:
+                    five -= 3 
+                
+                else:
+                    return False
+        return True
+
