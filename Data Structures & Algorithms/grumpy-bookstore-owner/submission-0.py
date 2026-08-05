37694087eee9a8742c17f40a50da88640b9270d3
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-05 18:39:54 +0530

    Add: grumpy-bookstore-owner - submission-0

---
+++ Data Structures & Algorithms/grumpy-bookstore-owner/submission-0.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        '''
+        Pattern - Fixed Sliding Window
+
+        TC - O(N)
+        SC - O(1)
+        '''
+
+        normal_total = 0
+        for i in range(len(grumpy)):            
+            if grumpy[i] == 0:
+                normal_total += customers[i]
+
+        window = minutes
+        left = 0
+        ans = 0
+        cur = 0
+        grumpy_window_total = 0
+
+        for right in range(len(grumpy)):
+            cur += customers[right]
+
+            if grumpy[right] == 0:
+                grumpy_window_total += customers[right]
+
+            if right - left + 1 == window:
+                ans = max(ans, cur + normal_total - grumpy_window_total)
+                cur -= customers[left]
+
+                if grumpy[left] == 0:
+                    grumpy_window_total -= customers[left]
+
+                left += 1          
+
+        return ans
